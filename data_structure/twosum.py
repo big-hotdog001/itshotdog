b471a725fbf2922bd132c5b72c858584bafe06a4
Y <superman136972@gmail.com>
2024-11-23 02:19:38 +0800

    11/23

--- data_structure/twosum.py
+++ data_structure/twosum.py
@@ -1,1 +1,12 @@
-print("hi")
+## Solution of mine
+def findSums(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return nums[i], nums[j]
+    
+n = int(input())
+nums = [2, -3, 7, 11, 15, 17]
+num1, num2 = findSums(nums, n)
+print(num1, num2)
+
